e7455bfb69036f0592ee1fc3e5cb4b4ca9c75634
MugaboDenys <mugabodan2020@gmail.com>
2023-03-15 11:39:38 +0200

    fixed deploy issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
-import { data } from "./data"
 import Split from "react-split"
 import {nanoid} from "nanoid"
 import { useEffect, useState } from "react"
